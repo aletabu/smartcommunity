ed0b4a8ce31de3eb4b556aa31138a815aedfeaaa
Constanza Areco <connieareco@MacBook-Pro-de-Constanza.local>
2014-01-12 17:25:04 -0300

    Initial Commit

---
+++ SmartCommunity/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SmartCommunity
+//
+//  Created by Constanza Areco on 12/01/14.
+//  Copyright (c) 2014 Topgrade Solutions S.A. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SmartCommunity/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SmartCommunity
+//
+//  Created by Constanza Areco on 12/01/14.
+//  Copyright (c) 2014 Topgrade Solutions S.A. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
